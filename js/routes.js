18530cdc2a71f78d43caf6969de26e09e4425a7f
pinkatong06 <petros.velissaratos@gmail.com>
2026-08-23 15:31:02 +0300

    Add routes for List and Leaderboard components

---
+++ js/routes.js
@@ -0,0 +1,9 @@
+import List from './pages/List.js';
+import Leaderboard from './pages/Leaderboard.js';
+
+
+export default [
+    { path: '/', component: List },
+    { path: '/leaderboard', component: Leaderboard },
+    { path: '/roulette', component: Roulette },
+];
